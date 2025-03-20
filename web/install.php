2fadf26dc8ae8fb9994143dff89aeaabaf3fae66
jeuhmeuh <grammator@gmail.com>
2025-03-20 21:19:15 +0100

    Update install.php
    mise a jour fichier install

--- web/install.php
+++ web/install.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="style.css">
 <?php
 // Vérification de la présence du fichier cfg.php
 if (file_exists("cfg.php")) {
@@ -10,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
     $dbname = $_POST["dbname"];
-    $sqlFile = "database.sql"; // Chemin vers votre fichier SQL
+    $sqlFile = "produits_chimiques.sql"; // Chemin vers votre fichier SQL
 
     // Connexion à MariaDB
     $conn = new mysqli($host, $username, $password, "", $port);
@@ -58,18 +59,26 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <title>Installateur</title>
 </head>
 <body>
+<H2>Installation du système d'inventaire de produits chimiques :</H2>
+<br>
+<br>
     <form method="POST">
         <label>Hôte :</label>
-        <input type="text" name="host" required><br>
+        <input type="text" name="host" value="127.0.0.1"required>
+		(habituellement 127.0.0.1)<br>
         <label>Port :</label>
-        <input type="number" name="port" required><br>
+        <input type="number" name="port" value=3306 required>
+		(port d'accès à MariaDB 3306 par defaut)
+		<br>
         <label>Identifiant :</label>
         <input type="text" name="username" required><br>
         <label>Mot de passe :</label>
         <input type="password" name="password" required><br>
         <label>Nom de la base de données :</label>
-        <input type="text" name="dbname" required><br>
-        <button type="submit">Installer</button>
+        <input type="text" name="dbname" required>
+		(nom souhaité pour la base de donées)
+		<br>
+        <button class="styled-button" type="submit">Installer</button>
     </form>
 </body>
 </html>
